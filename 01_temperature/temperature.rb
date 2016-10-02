a2477c4b16b63ca2da1939a6b7c023e4164d7c32
Thomas Countz <thomascountz@gmail.com>
2016-10-02 02:35:08 +0000

    completed 02_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc(fahrenheit) 
+   celsius = (fahrenheit.to_f - 32) * (5.00/9.00).to_f
+   celsius.to_f
+end
+
+def ctof(celsius)
+    fahrenheit = celsius.to_f * 9.00/5.00 + 32
+    fahrenheit.to_f
+end
+
